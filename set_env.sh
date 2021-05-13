bcb645dd00d850d1778218e20fccd494191bb0a4
naohiro <nao114computer@gmail.com>
2021-05-14 00:48:17 +0900

    fix

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 
 touch .env
 
-MYSQL_ROOT_PASSWORD = `cat /dev/urandom | tr -dc "[:alnum:]" | head -c 30`
+MYSQL_ROOT_PASSWORD=`cat /dev/urandom | tr -dc "[:alnum:]" | head -c 30`
 echo "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" >> .env
-MYSQL_PASSWORD = `cat /dev/urandom | tr -dc "[:alnum:]" | head -c 30`
+MYSQL_PASSWORD=`cat /dev/urandom | tr -dc "[:alnum:]" | head -c 30`
 echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> .env
